385c70234b056fccb3944d4f8d877e4c27812edb
MadaShindeInai <sergei.nepryahin.work@gmail.com>
2022-05-08 14:04:18 +0200

    feat: add validation

--- app/routes/jokes/new.tsx
+++ app/routes/jokes/new.tsx
@@ -1,13 +1,44 @@
 import type { ActionFunction } from "@remix-run/node";
 import { redirect } from "@remix-run/node";
+import { useActionData } from "@remix-run/react";
 import { db } from "~/utils/db.server";
 
-export const action: ActionFunction = async ({ request }) => {
+const validateJokeName = (name: string) => {
+  if (name.trim().length < 3) {
+    return "Joke name must be at least 3 characters long";
+  }
+  if (name.trim().length > 20) {
+    return "Joke name must be at most 20 characters long";
+  }
+  return null;
+};
+
+const validateJokeContent = (content: string) => {
+  if (content.trim().length < 10) {
+    return "Joke content must be at least 10 characters long";
+  }
+  if (content.trim().length > 500) {
+    return "Joke content must be at most 500 characters long";
+  }
+  return null;
+};
+export const action: ActionFunction = async ({
+  request,
+}): Promise<Response | ActionData> => {
   const form = await request.formData();
   const name = form.get("name");
   const content = form.get("content");
   if (typeof name !== "string" || typeof content !== "string") {
-    throw new Error(`Form not submitted correctly.`);
+    return {
+      formError: `Form not submitted correctly.`,
+    };
+  }
+  const fieldErrors = {
+    name: validateJokeName(name),
+    content: validateJokeContent(content),
+  };
+  if (Object.values(fieldErrors).some(Boolean)) {
+    return { fieldErrors, fields: { name, content } };
   }
   const joke = await db.joke.create({
     data: { name, content },
@@ -15,22 +46,73 @@ export const action: ActionFunction = async ({ request }) => {
   return redirect(`/jokes/${joke.id}`);
 };
 
+type ActionData = {
+  formError?: string;
+  fieldErrors?: {
+    name: string | null;
+    content: string | null;
+  };
+  fields?: {
+    name?: string;
+    content?: string;
+  };
+};
+
 export default function NewJokeRoute() {
+  const actionData = useActionData<ActionData>();
   return (
     <div>
       <p>Add your own hilarious joke</p>
       <form method="post">
         <div>
           <label>
-            Name: <input type="text" name="name" />
+            Name:{" "}
+            <input
+              type="text"
+              defaultValue={actionData?.fields?.name}
+              name="name"
+              aria-invalid={Boolean(actionData?.fieldErrors?.name) || undefined}
+              aria-errormessage={
+                actionData?.fieldErrors?.name ? "name-error" : undefined
+              }
+            />
           </label>
+          {actionData?.fieldErrors?.name ? (
+            <p className="form-validation-error" role="alert" id="name-error">
+              {actionData.fieldErrors.name}
+            </p>
+          ) : null}
         </div>
         <div>
           <label>
-            Content: <textarea name="content" />
+            Content:{" "}
+            <textarea
+              defaultValue={actionData?.fields?.content}
+              name="content"
+              aria-invalid={
+                Boolean(actionData?.fieldErrors?.content) || undefined
+              }
+              aria-errormessage={
+                actionData?.fieldErrors?.content ? "content-error" : undefined
+              }
+            />
           </label>
+          {actionData?.fieldErrors?.content ? (
+            <p
+              className="form-validation-error"
+              role="alert"
+              id="content-error"
+            >
+              {actionData.fieldErrors.content}
+            </p>
+          ) : null}
         </div>
         <div>
+          {actionData?.formError ? (
+            <p className="form-validation-error" role="alert">
+              {actionData.formError}
+            </p>
+          ) : null}
           <button type="submit" className="button">
             Add
           </button>
